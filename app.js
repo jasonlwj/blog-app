ebb9eab8e74deeb19e2991d998e4a52761d851f9
Jason Liew <jason.97.liew@gmail.com>
2020-11-06 12:28:49 +1100

    Extract blog routes to separate module

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 
 logger.info('Connecting to MongoDB...')
 
@@ -27,22 +27,6 @@ mongoose
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
-})
-
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body)
-
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
