9d23c754c8f6ee710104280b4e58ed7a7c1421fc
Jason Liew <jason.97.liew@gmail.com>
2020-11-25 18:41:18 +1100

    Update blog_api.test.js to use test helper module

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,49 +3,12 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-const initialBlogs = [
-	{ 
-		title: 'Go To Statement Considered Harmful',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-		likes: 5,
-	},
-	{ 
-		title: 'Canonical string reduction',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-		likes: 12,
-	},
-	{ 
-		title: 'First class tests',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-		likes: 10,
-	},
-	{ 
-		title: 'TDD harms architecture',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-		likes: 0,
-	},
-	{ 
-		title: 'Type wars',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-		likes: 2,
-	}
-]
-
-const blogsInDb = async () => {
-	const blogs = await Blog.find({})
-	return blogs.map(blog => blog.toJSON())
-}
+const helper = require('./test_helper')
 
 beforeEach(async () => {
 	await Blog.deleteMany({})
 
-	for (let blog of initialBlogs) {
+	for (let blog of helper.initialBlogs) {
 		let blogObject = new Blog(blog)
 		await blogObject.save()
 	}
@@ -62,7 +25,7 @@ describe('when performing a GET request...', () => {
 	test('all blogs are returned', async () => {
 		const res = await api.get('/api/blogs')
 
-		expect(res.body).toHaveLength(initialBlogs.length)
+		expect(res.body).toHaveLength(helper.initialBlogs.length)
 	})
 
 	test('blog has an id property', async () => {
@@ -87,10 +50,10 @@ describe('when performing a POST request...', () => {
 			.expect(201)
 			.expect('Content-Type', /application\/json/)
 
-		const blogsAfterOperation = await blogsInDb()
+		const blogsAfterOperation = await helper.blogsInDb()
 		const blogTitles = blogsAfterOperation.map(blog => blog.title)
 
-		expect(blogsAfterOperation).toHaveLength(initialBlogs.length + 1)
+		expect(blogsAfterOperation).toHaveLength(helper.initialBlogs.length + 1)
 		expect(blogTitles).toContain('React patterns')
 	})
 
@@ -122,21 +85,21 @@ describe('when performing a POST request...', () => {
 			.expect(400)
 			.expect('Content-Type', /application\/json/)
 
-		const blogsAfterOperation = await blogsInDb()
-		expect(blogsAfterOperation).toHaveLength(initialBlogs.length)
+		const blogsAfterOperation = await helper.blogsInDb()
+		expect(blogsAfterOperation).toHaveLength(helper.initialBlogs.length)
 	})
 })
 
 describe('when performing a DELETE request...', () => {
 	test('a blog can be deleted', async () => {
-		const blogsAtStart = await blogsInDb()
+		const blogsAtStart = await helper.blogsInDb()
 		const blogToDelete = blogsAtStart[0]
 
 		await api
 			.delete(`/api/blogs/${blogToDelete.id}`)
 			.expect(204)
 		
-		const blogsAfterOperation = await blogsInDb()
+		const blogsAfterOperation = await helper.blogsInDb()
 		const blogTitles = blogsAfterOperation.map(blog => blog.title)
 
 		expect(blogTitles).not.toContain(blogToDelete.title)
@@ -145,7 +108,7 @@ describe('when performing a DELETE request...', () => {
 
 describe('when performing a PUT request...', () => {
 	test('a blog can be updated', async () => {
-		const blogsAtStart = await blogsInDb()
+		const blogsAtStart = await helper.blogsInDb()
 		const blogToUpdate = blogsAtStart[0]
 
 		const updatedBlog = {
